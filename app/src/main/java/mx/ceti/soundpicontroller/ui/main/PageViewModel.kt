e23e87cd12f4f66aa12fb4304dec42296c724191
SealtielFreak <Die.Seal_Valderrama@outlook.com>
2024-01-22 17:02:51 -0600

    Minor changes

--- app/src/main/java/mx/ceti/soundpicontroller/ui/main/PageViewModel.kt
+++ app/src/main/java/mx/ceti/soundpicontroller/ui/main/PageViewModel.kt
@@ -10,7 +10,7 @@ class PageViewModel : ViewModel() {
 
     private val _index = MutableLiveData<Int>()
     val text: LiveData<String> = _index.map {
-        "Hello world from section: $it"
+        "Hello world from section: $it!"
     }
 
     fun setIndex(index: Int) {
--- app/src/main/java/mx/ceti/soundpicontroller/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/mx/ceti/soundpicontroller/ui/main/SectionsPagerAdapter.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import mx.ceti.soundpicontroller.DelayEffect
+import mx.ceti.soundpicontroller.FuzzEffect
 import mx.ceti.soundpicontroller.R
+import mx.ceti.soundpicontroller.ReverbEffect
+import mx.ceti.soundpicontroller.VibratoEffect
 
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+
+private val MAP_EFFECT = mapOf<Int, Fragment>(
+    R.string.tab_text_1 to (DelayEffect as Fragment),
+    R.string.tab_text_2 to (ReverbEffect as Fragment),
+    R.string.tab_text_3 to (VibratoEffect as Fragment),
+    R.string.tab_text_4 to (FuzzEffect as Fragment),
 )
 
 /**
@@ -19,17 +26,26 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment.
-        return PlaceholderFragment.newInstance(position + 1)
+        return when(position) {
+            0 -> DelayEffect.newInstance("", "")
+            1 -> ReverbEffect.newInstance("", "")
+            2 -> VibratoEffect.newInstance("", "")
+            3 -> FuzzEffect.newInstance("", "")
+            else -> DelayEffect.newInstance("", "")
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        return when(position) {
+            0 -> "Delay"
+            1 -> "Reverb"
+            2 -> "Vibrato"
+            3 -> "Fuzz"
+            else -> ""
+        }
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return 4
     }
 }
